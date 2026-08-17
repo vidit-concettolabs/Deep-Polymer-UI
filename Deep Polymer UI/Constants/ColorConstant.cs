ef7583ebe4ddfc7fbed9e23d18d6bf029e620ce8
Vidit Hirpara <vidit.hirpara@concettolabs.com>
2026-08-17 14:57:30 +0530

    - Manage common bg flag wise. - Implement navmenu design falg wise.

--- Deep Polymer UI/Constants/ColorConstant.cs
+++ Deep Polymer UI/Constants/ColorConstant.cs
@@ -8,10 +8,12 @@ public static class ColorConstant
     // Santej Colors
     public const string SantejPrimaryColor = "#ff0000";
     public const string SantejPrimaryBg = "#FFE5E5";
+    public const string SantejPageBg = "#F2F2F2";
 
     // Hajipur Colors
     public const string HajipurPrimaryColor = "#0405AB";
     public const string HajipurPrimaryBg = "#EBEBFF";
+    public const string HajipurPageBg = "#F5F5FF";
 
     // Logos
     public const string SantejLogo = "assets/image/logo_santej.svg";
@@ -20,5 +22,6 @@ public static class ColorConstant
     // Dynamic resolution helpers
     public static string PrimaryColor => BranchFlag == 2 ? HajipurPrimaryColor : SantejPrimaryColor;
     public static string PrimaryBg => BranchFlag == 2 ? HajipurPrimaryBg : SantejPrimaryBg;
+    public static string PageBg => BranchFlag == 2 ? HajipurPageBg : SantejPageBg;
     public static string LogoUrl => BranchFlag == 2 ? HajipurLogo : SantejLogo;
 }
